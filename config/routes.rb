6fc60cef7723db58d0bae78e5848d6fc73d1f1dd
shhuang123 <sharonhuang923@gmail.com>
2019-04-18 01:53:48 +0000

    adding comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Grammable::Application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+   resources :grams do
+    resources :comments, only: :create
+  end
 end
